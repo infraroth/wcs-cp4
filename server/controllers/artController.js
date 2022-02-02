0c8dc3b6e4f751d11333b788d155d3607cbf1fdd
Julien Roth <julien@infraroth.com>
2022-02-02 18:32:31 +0100

    Completed CRUD

--- server/controllers/artController.js
+++ server/controllers/artController.js
@@ -1,19 +1,105 @@
 import express from "express";
-//import Joi from "joi";
+import Joi from "joi";
 import Art from "../models/artModel.js";
 //import slugify from "slugify";
-//import multer from "multer";
-// import path from "path";
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, callBack) => {
+    callBack(null, "public");
+  },
+  filename: (req, file, callBack) => {
+    callBack(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+const schemaArt = Joi.object({
+  name: Joi.string().max(255).required(),
+  image: Joi.string().max(255).required(),
+  description: Joi.string().required(),
+});
+
 router.get("/", async (req, res) => {
   try {
-    const brand = await Art.getAll();
-    if (brand) return res.status(200).json(brand);
+    const art = await Art.getAll();
+    if (art) return res.status(200).json(art);
+    else res.status(404).json({ message: "Art not found" });
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
+
+router.get("/user/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const art = await Art.getAllByUser(id);
+    if (art) return res.status(200).json(art);
+    else res.status(404).json({ message: "Art not found" });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.post("/", upload.single("image"), async (req, res) => {
+  const { name, description, user_id } = req.body;
+  const image = `http://localhost:8000/images/${req.files[0].filename}`;
+  try {
+    const { error, value } = await schemaArt.validate({
+      name,
+      image,
+      description,
+    });
+    const lastInsertId = await Art.createNew(value);
+    console.log(lastInsertId);
+    if (lastInsertId) {
+      const newUserArt = await Art.addUserToArt(user_id, lastInsertId);
+      const newArt = await Art.getOneById(lastInsertId);
+      res.status(201).json({ artwork_id: newArt.id, user_artwork: newUserArt.insertId });
+    } else res.status(422).json({ message: error.message });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.put("/:id", upload.single("image"), async (req, res) => {
+  const { name, description } = req.body;
+  const image = `http://localhost:8000/images/${req.files[0].filename}`;
+  try {
+    const { error, value } = await schemaArt.validate({
+      name,
+      description,
+      image,
+    });
+    const artUpdate = await Brand.updateBrand(req.params.id, value);
+    if (artUpdate) res.status(201).json(value);
+    else res.status(422).json({ message: error.message });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await Art.deleteById(id);
+    result
+      ? res.json({ message: `ArtId ${id} has been deleted !` })
+      : res.status(404).json({ message: "Art not found" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
 
 export default router;
--- server/models/artModel.js
+++ server/models/artModel.js
@@ -14,4 +14,71 @@ const getAll = () => {
   })
 }
 
-export default {getAll}
+const getAllByUser = (id) => {
+  return new Promise((resolve, reject) => {
+      dbConnect.query(`SELECT a.id, a.name, a.description, a.image AS art, ua.user_id
+      FROM user_artwork AS ua
+      LEFT JOIN artwork AS a ON a.id = ua.artwork_id
+      LEFT JOIN user AS u ON u.id = ua.user_id
+      WHERE ua.user_id = ?
+      GROUP BY a.id`, id, (err, results) => {   
+          if (err) reject(err);
+          else resolve(results);
+      })
+  })
+}
+
+const getOneById = (id) => {
+  return new Promise((resolve, reject) => {
+      dbConnect.query(`SELECT a.id, a.name, a.description, a.image,
+      JSON_REMOVE(JSON_OBJECTAGG(IFNULL(u.id, 'null__'), u.username), '$.null__') AS user
+      FROM artwork AS a
+      LEFT JOIN user_artwork AS ua ON a.id = ua.artwork_id
+      LEFT JOIN user AS u ON u.id = ua.user_id
+      WHERE a.id = ?
+      GROUP BY a.id`, id, (err, result) => {
+          if (err) reject(err);
+          else resolve(result[0]);
+      })
+  })
+}
+
+const createNew = (art) => {
+  const { name, description, image } = art;
+  return new Promise((resolve, reject) => {
+      dbConnect.query('INSERT INTO artwork (name, description, image) VALUES (?, ?, ?)',
+          [name, description, image], (err, result) => {
+              if (err) reject(err);
+              else resolve(result.insertId);
+          })
+  })
+}
+
+const addUserToArt = (user_id, artwork_id) => {
+  return new Promise((resolve, reject) => {
+      dbConnect.query('INSERT INTO user_artwork (user_id, artwork_id) VALUES (?, ?)', [user_id, artwork_id], (err, result) => {
+          if (err) reject(err);
+          else resolve(result);
+      })
+  })
+}
+
+const updateArt = (id, art) => {
+  return new Promise((resolve, reject) => {
+      dbConnect.query('UPDATE artwork SET ? WHERE id = ?', [art, id], (err, result) => {
+          if (err) reject(err);
+          else resolve(result);
+      })
+  })
+}
+
+const deleteById = (id) => {
+  return new Promise((resolve, reject) => {
+      dbConnect.query('DELETE FROM artwork WHERE id = ?', id, (err, result) => {
+          if (err) reject(err);
+          else resolve(result.affectedRows);
+      })
+  })
+}
+
+export default {getAll, createNew, getOneById, addUserToArt, getAllByUser, updateArt, deleteById}
