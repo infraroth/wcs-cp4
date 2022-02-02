b66b55db576e16e5fd44a745bd3aa82db264ed06
Julien Roth <julien@infraroth.com>
2022-02-02 18:45:16 +0100

    Added GET for user infos

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -17,6 +17,15 @@ const schemaUpdateUser = Joi.object({
     username: Joi.string().max(255)
 });
 
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+      const user = await User.findById(id);
+      if (user) return res.status(200).json({ id: user.id, email: user.email, username: user.username });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  });
 
 router.post('/signup', async (req, res) => {
     const { email, password, username } = req.body;
